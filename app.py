0fcb66660a43a54e8deceebe969a88d6dabf3b89
Ahmed Gomaa <114065058+ahmedgomaa2004@users.noreply.github.com>
2025-10-30 00:27:35 +0300

    Update app to run on configurable host and port

--- app.py
+++ app.py
@@ -630,4 +630,5 @@ def report(batch_id):
 # =========================
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
